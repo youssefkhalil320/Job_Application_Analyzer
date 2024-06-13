1ecbf396a05126b73aa0479cc8e25d0d0dfab258
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-06-13 16:22:08 +0300

    get_questions function

--- app.py
+++ app.py
@@ -1,36 +1,57 @@
 from modules.pdf_parser import get_full_resume_text
 from modules.gemini_chat import gemini_chat, model
-from modules.prompts import summary_prompts
+from modules.prompts import summary_prompts, test_prompts
 from modules.export_report import export_summary_to_pdf
 from collections import defaultdict
 import time
+
+num_questions_per_prompt = 2
+
 
 chat = model.start_chat(history=[])
 
 data = get_full_resume_text("./examples/Khalil_sResumeAPR2024.pdf")
 
-target_prompts = ["years_of_experience_prompt",
-                  "education_prompt", "job_titles_prompt", "skills_prompt"]
+questions_prompts_keys = ["years_of_experience_prompt",
+                          "education_prompt", "job_titles_prompt", "skills_prompt"]
+
+test_prompts_keys = ["Questions_according_to_jobs",
+                     "Questions_related to_skills"]
 
 candidate_details = defaultdict(str)
 
 response = chat.send_message(summary_prompts["description_prompt"])
+time.sleep(15)
 response = chat.send_message(data)
+time.sleep(15)
 
 
-def get_summary(data):
-    for prompt in target_prompts:
+def get_summary():
+    for prompt in questions_prompts_keys:
         response = chat.send_message(summary_prompts[prompt]).text
         print(response)
         candidate_details[prompt] = response
         time.sleep(30)
 
-    return candidate_details
+
+def get_questions():
+    score = 0
+    for prompt in test_prompts_keys:
+        for i in range(num_questions_per_prompt):
+            question = chat.send_message(test_prompts[prompt]).text
+            print(question)
+            answer = input("Write your answer please:  ")
+            evaluation = chat.send_message(answer).text
+            print(evaluation)
+            if "correct" in evaluation.lower():
+                score += 1
+
+            time.sleep(30)
+
+    return score
 
 
-def get_questions(data):
-    pass
-
-
-candidate_details_dict = get_summary(data)
-export_summary_to_pdf(candidate_details_dict, "./Reports/Khalil_details.pdf")
+get_summary()
+score = get_questions()
+print(score)
+export_summary_to_pdf(candidate_details, "./Reports/Khalil_details.pdf")
